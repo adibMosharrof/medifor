e514a1168248c639cfc5a6cef0a0159144bc8113
Adib Mosharrof <adib.mosharrof@gmail.com>
2019-07-21 00:02:27 -0700

    getting the image with no score region

--- localization/metrics/metrics.py
+++ localization/metrics/metrics.py
@@ -1,5 +1,6 @@
 import os
 from PIL import Image
+import cv2 
 import numpy as np
 from sklearn.metrics import matthews_corrcoef
 import matplotlib.pyplot as plt
@@ -12,13 +13,41 @@ class Metrics(object):
         self.thresholds = [0.5, 0.7]
         data = self.read_data(self.data_path)
         self.get_average_score(data)
+        #self.get_image_with_noscore_region(data[0])
     
     def get_average_score(self, data):
         scores = 0
         for d in data:
-            scores += self.get_image_score(d.ref, d.sys)     
+            img_with_noscore = self.get_image_with_noscore_region(d)
+            scores += self.get_image_score(img_with_noscore, d.sys)     
         return scores/len(data)
     
+    def get_image_with_noscore_region(self, data):
+        '''
+            Dilates the image and returns a new image that has 3 types of pixels
+            0 : manipulated regions
+            100: no score region
+            255: non-manipulated regions
+        '''
+        kernel = np.ones((5,5), np.uint8) 
+        img_dilation = None
+        
+        img = np.array(data.ref.convert('L'))
+        img = np.where(img < 255, 0, img)
+
+        img_dilated = cv2.dilate(img, kernel, iterations=4)
+
+        img_dilated_region = img_dilated - img
+        img_dilated_region = np.where(img_dilated_region == 255, 100, 255)
+        
+        result = img + img_dilated_region
+        result = np.where(result == 255, 0 , result)
+        result = np.where(result == 510, 255 , result)
+        
+#         plt.imshow(result, cmap='gray')
+#         plt.show()
+        return result
+      
     def get_image_score(self, ref, sys):
         max_score = np.NINF
         for t in self.thresholds:
@@ -65,7 +94,21 @@ class Metrics(object):
     
     def get_mcc_score(self, predictions, manipulations):
         return matthews_corrcoef(manipulations, predictions)
-                
+
+    
+    
+    def read_data(self, data_path):
+        folders = self.get_folders(data_path)
+        data = []
+        for folder in folders:
+            res = self.get_images(folder)
+            data.append(MediforData(res['ref'], res['sys']))
+        return data
+    
+    def get_folders(self, data_path):
+        subfolders = [f.path for f in os.scandir(data_path) if f.is_dir() ]
+        return subfolders
+    
     def get_images(self, folder):
         ref_path = folder+'/refMask.png'
         sys_path = folder+'/sysMask.png'
@@ -78,21 +121,7 @@ class Metrics(object):
         except:
             print('failed to open: %s' % sys_path)
         
-        return {'ref':ref_image, 'sys':sys_image}    
-    
-    def get_folders(self, data_path):
-        subfolders = [f.path for f in os.scandir(data_path) if f.is_dir() ]
-        return subfolders
-    
-    def read_data(self, data_path):
-        folders = self.get_folders(data_path)
-        data = []
-        for folder in folders:
-            res = self.get_images(folder)
-            data.append(MediforData(res['ref'], res['sys']))
-        return data
-
-        
+        return {'ref':ref_image, 'sys':sys_image}     
     
     
    
