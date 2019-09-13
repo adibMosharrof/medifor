5dca843cd5391cae5655ee3368ebff3c00136659
Adib Mosharrof <adib.mosharrof@gmail.com>
2019-09-13 16:31:43 -0700

    added data size to config file

--- localization/metrics/img_ref_builder.py
+++ localization/metrics/img_ref_builder.py
@@ -18,6 +18,7 @@ class ImgRefBuilder:
     sys_data_path = base_data_path +"MFC18_EvalPart1/c8-lgb_local_40_nb_a/mask/"
     image_ref_csv_path = None
     my_logger = None
+    data_size = None
     
     def __init__(self, config_json, env_json, logger):
         self.my_logger = logger
@@ -29,11 +30,13 @@ class ImgRefBuilder:
         self.ref_data_path = '{}{}'.format(self.current_data_path, env_path["target_mask"])
         model_type = config_json["default"]["model_type"]
         self.sys_data_path = '{}{}'.format(env_path["model_sys_predictions"], env_path["model_type"][model_type])
+        
+        self.data_size = self.get_data_size(env_json)
     
     def get_img_ref_data(self):
         img_refs = self.get_img_ref()
         data = []
-        for img_ref in img_refs[0:1]:
+        for img_ref in img_refs[0:self.data_size]:
             result = self.read_img_ref(img_ref)
             data.append(MediforData(result['ref'], result['sys'], ''))
         return data
@@ -73,6 +76,12 @@ class ImgRefBuilder:
         
     def extract_ref_mask_file_name(self, text):
         return re.search("(?<=reference\/manipulation-image\/mask\/).*(?=.ccm.png)", text).group()
+
+    def get_data_size(self, env_json):
+        try:
+          self.data_size = int(env_json["data_size"])
+        except ValueError:
+          self.data_size = None
 
 class ImgRefs:
     sys_mask_file_name = None
