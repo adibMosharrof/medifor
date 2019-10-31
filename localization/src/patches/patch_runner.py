6c552551d6270d633265cb0b3b6cf4607240a634
Adib Mosharrof <adib.mosharrof@gmail.com>
2019-10-30 20:39:43 -0700

    created folders and files for patches

---
+++ localization/src/patches/patch_runner.py
@@ -0,0 +1,13 @@
+
+class PatchRunner:
+    
+    def __init__(self):
+        a=1
+        
+    def start(self):
+        a = 1
+            
+            
+if __name__ == '__main__':
+    r = Runner()
+    r.start()
