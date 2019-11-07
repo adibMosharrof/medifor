b02ddc9bbb609eb4a5e46a4b34ab1a360b740f80
adibmosharrof <adib.mosharrof@gmail.com>
2019-11-07 13:07:31 -0800

    read opt

--- localization/src/patches/read_opt.py
+++ localization/src/patches/read_opt.py
@@ -1,8 +1,10 @@
 import os
+import sys
+sys.path.append('..')
 import cv2
 import time
 from multiprocessing import Pool, freeze_support
-
+from shared.image_utils import ImageUtils
 
 def read_all(dir_path, starting_index, ending_index):
     img_names = os.listdir(dir_path)[starting_index:ending_index]
@@ -14,10 +16,11 @@ def read_all(dir_path, starting_index, ending_index):
     return imgs
 
 def read_all_mult(dir_path, starting_index, ending_index):
-    img_names = os.listdir(dir_path)[starting_index:ending_index]
+    img_names = [ os.path.join(dir_path, a) for a in os.listdir(dir_path)[starting_index:ending_index]]
+    
     with Pool() as p:
         imgs = p.map(read, img_names)
-    print(len(imgs))
+    return imgs
 
 def read(path):
     return cv2.imread(path)
@@ -27,14 +30,17 @@ if __name__ == '__main__':
     freeze_support()
     starting_index = 0
     ending_index = 60
-    dir_path = '../../outputs/patches/128_1/target_image'
+    dir_path = '../../outputs/patches/128_4/target_image'
+#     dir_path = '../../data/model_sys_predictions/c8-lgb_local_40_nb_a/mask'
     
     t1 = time.perf_counter()
-    read_all(dir_path,starting_index, ending_index)
+    van = read_all(dir_path,starting_index, ending_index)
     t2 = time.perf_counter()
     print(f'vanilla {round((t2-t1), 3)} seconds')
     
     t1 = time.perf_counter()
-    read_all_mult(dir_path,starting_index, ending_index)
+    mult = read_all_mult(dir_path,starting_index, ending_index)
     t2 = time.perf_counter()
     print(f'mult {round((t2-t1), 3)} seconds')
+    
+    ImageUtils.display_multiple(van[0], mult[0])
