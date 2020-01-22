1a3b7f9a67d03bfb4ea492a036a1bd74a5dc91f1
Adib Mosharrof <adib.mosharrof@gmail.com>
2020-01-22 14:18:26 -0800

    removed string errors

--- localization/src/predictions/config/config_loader.py
+++ localization/src/predictions/config/config_loader.py
@@ -60,7 +60,7 @@ class ConfigLoader():
     
     @staticmethod
     def print_config(config):
-        print(f"traing data size {config.train_data_size}" )
-        print(f"test data size {config.ending_index - config.starting_index - config.train_data_size}" )
-        print(f"Model name {config.model_name}")
+        print(f"traing data size {config['train_data_size']}" )
+        print(f"test data size {config['ending_index'] - config['starting_index'] - config['train_data_size']}" )
+        print(f"Model name {config['model_name']}")
         
