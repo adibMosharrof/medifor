e40f7756c9ef64326c9f64ac7e2b4a65fe270f72
Adib Mosharrof <adib.mosharrof@gmail.com>
2020-01-22 14:25:04 -0800

    made layers a parameter for unet model

--- localization/src/architectures/unet.py
+++ localization/src/architectures/unet.py
@@ -26,12 +26,30 @@ class UNet():
         c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(c)
         return c
     
-    def get_model(self, image_size, num_indicators, layers=5):
+    def get_model(self, image_size, num_indicators, layers=3):
         f = np.around(np.geomspace(image_size/2**(layers-1), image_size, num=layers)).astype('uint8')
 #         f = [8, 16, 32, 64, 128]
         inputs = keras.layers.Input((image_size, image_size, num_indicators))
         
-        p0 = inputs
+        p=[None]* layers
+        p[0] = inputs
+        c=[None]* layers
+        for i in range(0,layers-1):
+            c[i+1], p[i+1] = self.down_block(p[i], f[i])
+
+        bn = self.bottleneck(p[layers-1], f[layers-1])
+        u=[None]* layers
+        u[0] = bn
+        for i in range(0,layers-1):
+            u[i+1] = self.up_block(u[i], c[layers-i-1], f[layers-i-2])
+            
+        outputs = keras.layers.Conv2D(1, (1, 1), padding="same", activation="sigmoid")(u[layers-1])
+        outputs = outputs[:,:,:,-1]
+        model = keras.models.Model(inputs, outputs)
+        model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["acc"])
+        return model
+        
+        
         c1, p1 = self.down_block(p0, f[0]) #128 -> 64
         c2, p2 = self.down_block(p1, f[1]) #64 -> 32
         c3, p3 = self.down_block(p2, f[2]) #32 -> 16
