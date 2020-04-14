d72304f051c79a76804ed34867118bc99f049121
Adib Mosharrof <adib.mosharrof@gmail.com>
2020-04-13 22:29:18 -0700

    deleted unnecessary stuff in config for csv to image

--- localization/src/csv_to_image/config/config_loader.py
+++ localization/src/csv_to_image/config/config_loader.py
@@ -17,15 +17,6 @@ class ConfigLoader():
         parser.add_argument('-ei','--ending_index', type=int,
                     default=json_config['ending_index'],help='Ending Index')
         
-        parser.add_argument('-dy','--dilate_y', type=str,
-                    default=json_config['tuning']['dilate_y'],help='Dilate y')
-
-        parser.add_argument('-bb','--black_border_y', type=str,
-                    default=json_config['tuning']['black_border_y'],help='Black Border y')    
-
-        parser.add_argument('-dybb','--dilate_y_black_border_y', type=str,
-                    default=json_config['tuning']['dilate_y_black_border_y'],help='Dilate y and Black Border y')    
-
         parser.add_argument('-d','--data_year', type=str,
                     default=json_config['data_year'],help='Dataset')
         
@@ -39,10 +30,6 @@ class ConfigLoader():
                     default=json_config['patch_shape'],help='Patch Shape')
 
         config = vars(parser.parse_args())
-        
-        config['tuning'] = {'dilate_y':bool(config['dilate_y']), 'black_border_y':bool(config['black_border_y']), 'dilate_y_black_border_y':bool(config['dilate_y_black_border_y'])}
-        for param in ["black_border_y", "dilate_y", 'dilate_y_black_border_y']:
-            del config[param]
             
         json_config.update(config)
         ConfigLoader.print_config(json_config)
@@ -53,5 +40,4 @@ class ConfigLoader():
         print(f"data prefix {config['data_prefix']}")
         print(f"data size {config['ending_index'] - config['starting_index']}" )
         print(f"Image downscale factor {config['image_downscale_factor']}")
-        print(f"Tuning {config['tuning']}")
         
